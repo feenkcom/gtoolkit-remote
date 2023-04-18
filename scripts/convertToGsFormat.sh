60e3fd9246f095e208ed885299f43fcfdba2f4ed
Alistair Grant <akgrant@gmail.com>
2023-04-18 16:37:30 +0200

    Update gtoolkit-remote.gs

--- scripts/convertToGsFormat.sh
+++ scripts/convertToGsFormat.sh
@@ -3,14 +3,28 @@
 ### Requires loginSystemUser.topaz be configured with the correct credentials.
 ### Exits with 0 if success, topaz status if failed.
 
-gt4GemstoneHome=${ROWAN_PROJECTS_HOME}/gtoolkit-remote
+set -e
+
+if [ -z "$ROWAN_PROJECTS_HOME" ]
+then
+	echo "ROWAN_PROJECTS_HOME must be defined"
+	exit 1
+fi
+
+if [ -z "$STONE" ]
+then
+	echo "STONE must be defined"
+	exit 1
+fi
+
+repositoryDir=${ROWAN_PROJECTS_HOME}/gtoolkit-remote
 ## Topaz refuses to exit from script if input is stdin, so redirect from /dev/zero
-topaz -l -I ${gt4GemstoneHome}/scripts/loginSystemUser.topaz  -S ${gt4GemstoneHome}/scripts/convertToGsFormat.topaz < /dev/zero
+topaz -l -I ${repositoryDir}/scripts/loginSystemUser.topaz  -S ${repositoryDir}/scripts/convertToGsFormat.topaz < /dev/zero
 status=$?
 if [ $status != 0 ]
 then
         echo !!!!!!!!!!!!!!!!
-        echo Failed to export gtoolkit-remote
+        echo Failed to export $repositoryDir
         echo !!!!!!!!!!!!!!!!
         exit $status
 fi
