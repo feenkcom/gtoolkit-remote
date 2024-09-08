32f986e3f366f6569c87f4c61f6e7901c9706fe6
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2024-09-08 15:29:47 -0700

    add guard statements for required ROWAN_PROJECTS_HOME and STONE environment variables

--- scripts/installGtoolkitRemote.sh
+++ scripts/installGtoolkitRemote.sh
@@ -2,6 +2,20 @@
 ### Install gtoolkit-remote from Tonel files into a Rowan-enabled stone
 ### Exits with 0 if success, 1 if failed
 
+set -e
+
+if [ -z "$ROWAN_PROJECTS_HOME" ]
+then
+	echo "ROWAN_PROJECTS_HOME must be defined"
+	exit 1
+fi
+
+if [ -z "$STONE" ]
+then
+	echo "STONE must be defined"
+	exit 1
+fi
+
 gt4GemstoneHome=${ROWAN_PROJECTS_HOME}/gtoolkit-remote
 ## Topaz refuses to exit from script if input is stdin, so redirect from /dev/zero
 topaz -l -I ${gt4GemstoneHome}/scripts/loginSystemUser.topaz  -S ${gt4GemstoneHome}/scripts/installGtoolkit-remote.topaz < /dev/zero
